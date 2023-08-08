6891420c8bfe883e7a5439ef7c0f17b547b53bf4
Sharmukhan <sharmukhan@gmail.com>
2023-08-08 14:02:29 +0600

    Initial Commit

---
+++ Tabel View DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabel View DZ
+//
+//  Created by Kymbat Sharmukhan on 08.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
